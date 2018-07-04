a469b5a4fe909902bce9be9cd37440bd78cf9b61
amit singh <amit2k761@gmail.com>
2018-07-04 14:02:31 +0530

    initial commit

---
+++ server.js
@@ -0,0 +1,52 @@
+const express = require('express'); 
+const hbs = require('hbs');
+var app = express();
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine',hbs);
+
+app.use((req,resp,next)=>{
+var now= new Date().toString();
+
+console.log(`${now} :${req.method} ${req.url}`);
+
+next();
+});
+// app.use((req,resp,next)=>{
+// resp.render('maintenace.hbs');
+// });
+app.use(express.static(__dirname + '/public'));
+hbs.registerHelper('getCurrentYear',()=>{
+    return new Date().getFullYear();
+});
+// hbs.registerHelper('screamit',(text)=>{
+// return text.toUpperCase();
+// })
+app.get('/',(req,resp)=>{
+//resp.send(`<h1><em> hey what up </em> </h1>`);
+// express notices object and converts into json and sent into json
+resp.render('home.hbs',{
+    name : "amit",
+    likes:['biking', 'eating'],
+    pagetitle : 'Home page',
+    
+});
+});
+app.get('/about',(req, resp)=>{
+resp.render('about.hbs',{
+    pagetitle : 'about page',
+
+});
+});
+app.get('/bad',(req,resp)=>{
+    resp.send({
+        error : 'unable to send resp'
+    })
+})
+// bind the appl to a port on machine
+app.listen(3000,()=>{
+    console.log('server is up');
+});
+
+
+
+
